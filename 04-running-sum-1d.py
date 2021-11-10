9675080a3f839a361a3b166bdcfa104f1c3be7b5
Sai Pranav <rsaipranav92@gmail.com>
2021-11-09 18:05:42 -0600

    pa: adding running sum

---
+++ 04-running-sum-1d.py
@@ -0,0 +1,31 @@
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+# Constraints:
+# 1 <= nums.length <= 1000
+# -10^6 <= nums[i] <= 10^6
+
+# O(n)
+class Solution:
+    def runningSum(self, nums: list[int]) -> list[int]:
+        s = 0
+        r = []
+        for i in range(len(nums)):
+            s = s + nums[i]
+            r.append(s)
+        return r
+print(Solution().runningSum([1,2,3,4]))
+
+# O(n)
+# this is an unreadable solution but has list comprehension with function
+s = 0
+def sum(x: int) -> int:
+    global s # global var  is needed otherwise python picks up local variable and complains its not assigned
+    s = x+s
+    return s
+
+class Solution1:
+    def runningSum(self, nums: list[int]) -> list[int]:
+        global s
+        s = 0
+        return [sum(x) for x in nums] # list comprehension [expr with element for element in array] forms a new array
+print(Solution1().runningSum([1,2,3,4]))
